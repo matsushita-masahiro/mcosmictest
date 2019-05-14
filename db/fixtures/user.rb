3b54ed8d331269d80b3b3ba53cf41d94172c0954
EC2 Default User <ec2-user@ip-172-31-25-172.ap-southeast-1.compute.internal>
2019-05-14 10:38:40 +0000

    first 201905086

--- db/fixtures/user.rb
+++ db/fixtures/user.rb
@@ -1,1 +1,3 @@
-
+User.seed(:id,
+  {:id => 1, :email => "mstarcosmin@gmail.com", :name => "管理者", :name_kana => "かんりしゃ", :birthday => Date.today, :tel => "08037173718", :introducer => "管理者", :gender => "f",  }
+ )
